71784ea99ddcc1722ff14241779000d945f91d78
ksandov4 <ksandov4@ucsc.edu>
2023-11-02 16:11:10 -0700

    Changed to 8 Neuron Input

--- src/RandomDropout.v
+++ src/RandomDropout.v
@@ -1,44 +1,43 @@
 `default_nettype none
 
 module RandomDropout (
-    input wire clk,     //need a clock 
-    input wire reset,   //need reset as well
-    input wire enable,  //enable used to determine if dropout should occur
-    input wire [N-1:0]datain, //data in (could be a neuron, or could be a connection)
-    output wire [N-1:0]dataout //data out (if dropout occurs set this to 0, if not pass it back into data in)
+    input wire clk,      // need a clock
+    input wire reset,    // need reset as well
+    input wire enable,   // enable used to determine if dropout should occur
+    input wire [7:0] datain [0:7], // data in for 8 neurons
+    output wire [7:0] dataout [0:7] // data out for 8 neurons
 );
 
-parameter dropoutrate = 0.5; //dropout rate can be changed, it represents the likelihood that a given value will be set to 0. 
-parameter N = 8;
+parameter dropoutrate = 0.5; // dropout rate can be changed, it represents the likelihood that a given value will be set to 0.
 
-reg [31:0]randomvalues [0:N-1]; //array of random numbers, one per neuron 
-
+reg [31:0] randomvalues [0:7]; // array of random numbers, one per neuron
 
 always @(posedge clk or posedge reset) begin
     if (reset) begin
-        for (int i = 0; i < N; i = i + 1) begin
+        for (int i = 0; i < 8; i = i + 1) begin
             randomvalues[i] <= 32'h0;
         end
-        enable = 1'b0;
     end else if (enable) begin
-        for (int i = 0; i < N; i = i + 1) begin
+        for (int i = 0; i < 8; i = i + 1) begin
             randomvalues[i] <= $urandom; // Use $urandom for uniform distribution
         end
     end
 end
 
-always @* begin
-    for (int i = 0; i < N; i = i + 1) begin
-        if (reset) begin
-            dataout[i] = 32'h0; // Reset output to 0
-        else if (enable) begin
-            if (randomvalues[i] < dropoutrate * 2**32) begin
-                dataout[i] = 32'h0; // Apply dropout
-            end else begin
-                dataout[i] = datain[i]; // Pass data through
+generate
+    for (int i = 0; i < 8; i = i + 1) begin : dropout_block
+        always @* begin
+            if (reset) begin
+                dataout[i] = 8'h0; // Reset output to 0 for each neuron
+            else if (enable) begin
+                if (randomvalues[i] < dropoutrate * 2**32) begin
+                    dataout[i] = 8'h0; // Apply dropout
+                end else begin
+                    dataout[i] = datain[i]; // Pass data through
+                end
             end
         end
     end
-end
+endgenerate
 
 endmodule
