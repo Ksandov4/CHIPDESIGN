c187441f9298fb8f2064846d2e23caec24db0bee
ksandov4 <ksandov4@ucsc.edu>
2023-11-02 13:30:14 -0700

    Added an array of input neurons

--- src/RandomDropout.v
+++ src/RandomDropout.v
@@ -4,31 +4,39 @@ module RandomDropout (
     input wire clk,     //need a clock 
     input wire reset,   //need reset as well
     input wire enable,  //enable used to determine if dropout should occur
-    input wire datain, //data in (could be a neuron, or could be a connection)
-    output wire dataout //data out (if dropout occurs set this to 0, if not pass it back into data in)
+    input wire [N-1:0]datain, //data in (could be a neuron, or could be a connection)
+    output wire [N-1:0]dataout //data out (if dropout occurs set this to 0, if not pass it back into data in)
 );
 
 parameter dropoutrate = 0.5; //dropout rate can be changed, it represents the likelihood that a given value will be set to 0. 
+parameter N = 8;
 
-reg [31:0]randomvalue; //random value needed for the drpoout decision
+reg [31:0]randomvalues [0:N-1]; //array of random numbers, one per neuron 
 
-always @(posedge clk or posedge reset) begin 
+
+always @(posedge clk or posedge reset) begin
     if (reset) begin
-        randomvalue <= 32'h0; //if reset, we no longer need a random value set it to 0
+        for (int i = 0; i < N; i = i + 1) begin
+            randomvalues[i] <= 32'h0;
+        end
         enable <= 1'b0;
     end else if (enable) begin
-        randomvalue <= $urandom; //if enable, then generate the random number. 
+        for (int i = 0; i < N; i = i + 1) begin
+            randomvalues[i] <= $urandom; // Use $urandom for uniform distribution
+        end
     end
 end
 
-always @(posedge clk or posedge reset) begin 
-    if (reset) begin 
-        dataout <= 32'h0; //if reset then det the output data to 0
-    end else if (enable) begin 
-        if (randomvalue < dropoutrate * 2**32) begin //if random value is in the threshold preform the dropout
-            dataout <= 32'h0;
-        end else begin // if not loop the output data back into the input data. 
-            dataout <= datain;
+always @* begin
+    for (int i = 0; i < N; i = i + 1) begin
+        if (reset) begin
+            dataout[i] = 32'h0; // Reset output to 0
+        else if (enable) begin
+            if (randomvalues[i] < dropoutrate * 2**32) begin
+                dataout[i] = 32'h0; // Apply dropout
+            end else begin
+                dataout[i] = datain[i]; // Pass data through
+            end
         end
     end
 end
