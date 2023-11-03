fa720aec7694cdf9c72382501926f88a5026d9b9
ksandov4 <ksandov4@ucsc.edu>
2023-11-03 14:05:08 -0700

    Fixed some shit

--- src/RandomDropout.v
+++ src/RandomDropout.v
@@ -1,53 +1,308 @@
 `default_nettype none
 
-module RandomDropout (
-    input wire clk,      // need a clock
-    input wire reset,    // need reset as well
-    input wire enable,   // enable used to determine if dropout should occur
-    input wire [7:0] datain [0:7], // data in for 8 neurons
-    output reg [7:0] dataout [0:7] // data out for 8 neurons
+module dropout_module (
+    input wire clk,      // Clock signal
+    input wire rst_n,    // Reset signal
+    input wire ui_ena,   // Enable signal used to determine if dropout should occur
+    input wire [7:0] ui_in,
+    output wire [7:0] uo_out
 );
 
-parameter dropoutrate = 0.5; // dropout rate can be changed, it represents the likelihood that a given value will be set to 0.
+reg [7:0] dropout_weights [0:7]; // An array to store random dropout weights
+reg [7:0] dropout_enabled_weights [0:7]; // An array to store dropout-enabled weights
 
-reg [31:0] randomvalues [0:7]; // array of random numbers, one per neuron
+parameter dropout_rate = 8'b10000000; // Hyperparameter for dropout rate (e.g., 0.5 for 50% dropout)
 
-always @(posedge clk or posedge reset) begin
-    if (reset) begin
+// Generate random weights for each neuron
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        // Reset on active low reset
         for (int i = 0; i < 8; i = i + 1) begin
-            randomvalues[i] <= 32'h0;
+            dropout_weights[i] <= 8'b0; // Initialize to 0
         end
-    end else if (enable) begin
+    end else if (ui_ena) begin
+        // Generate random weights for each neuron
         for (int i = 0; i < 8; i = i + 1) begin
-            randomvalues[i] <= $urandom; // Use $urandom for uniform distribution
+            dropout_weights[i] <= $random; // Replace this with your own random weight generation logic
         end
     end
 end
 
-genvar i;
-generate
-    for (i = 0; i < 8; i = i + 1) begin : dropout_block
-        always @* begin
-            if (reset) begin
-                dataout[i] <= 8'h0; // Reset output to 0 for each neuron
-            end else if (enable) begin
-                if (randomvalues[i] < dropoutrate * 2**32) begin
-                    dataout[i] <= 8'h0; // Apply dropout
-                end else begin
-                    dataout[i] <= datain[i]; // Pass data through
-                end
+// Introduce dropout based on the hyperparameter dropout_rate
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        // Reset on active low reset
+        for (int i = 0; i < 8; i = i + 1) begin
+            dropout_enabled_weights[i] <= 8'b0; // Initialize to 0
+        end
+    end else if (ui_ena) begin
+        // Apply dropout based on the hyperparameter dropout_rate
+        for (int i = 0; i < 8; i = i + 1) begin
+            if ($random < dropout_rate) begin
+                // Set output to 0 based on dropout rate
+                dropout_enabled_weights[i] <= 8'b0;
+            end else begin
+                // Pass through the original weights
+                dropout_enabled_weights[i] <= dropout_weights[i];
             end
         end
     end
-endgenerate
+end
+
+// Output the selected weights
+assign uo_out[0] = dropout_enabled_weights[0];
+assign uo_out[1] = dropout_enabled_weights[1];
+assign uo_out[2] = dropout_enabled_weights[2];
+assign uo_out[3] = dropout_enabled_weights[3];
+assign uo_out[4] = dropout_enabled_weights[4];
+assign uo_out[5] = dropout_enabled_weights[5];
+assign uo_out[6] = dropout_enabled_weights[6];
+assign uo_out[7] = dropout_enabled_weights[7];
 
 endmodule
+
+
+// module dropout_module (
+//     input wire clk,      // Clock signal
+//     input wire rst_n,    // Reset signal
+//     input wire ui_ena,   // Enable signal used to determine if dropout should occur
+//     input wire [7:0] ui_in,
+//     output wire [7:0] uo_out
+// );
+
+// reg [7:0] dropout_weights [0:7]; // An array to store random dropout weights
+// wire [7:0] dropout_enabled_weights [0:7]; // An array to store dropout-enabled weights
+
+// parameter dropout_rate = 8'b10000000; // Hyperparameter for dropout rate (e.g., 0.5 for 50% dropout)
+
+// // Generate random weights for each neuron
+// always @(posedge clk or negedge rst_n) begin
+//     if (!rst_n) begin
+//         // Reset on active low reset
+//         for (i = 0; i < 8; i = i + 1) begin
+//             dropout_weights[i] <= 8'b0; // Initialize to 0
+//         end
+//     end else if (ui_ena) begin
+//         // Generate random weights for each neuron
+//         for (i = 0; i < 8; i = i + 1) begin
+//             dropout_weights[i] <= $random; // Replace this with your own random weight generation logic
+//         end
+//     end
+// end
+
+// // Introduce dropout based on the hyperparameter dropout_rate
+// always @(posedge clk or negedge rst_n) begin
+//     if (!rst_n) begin
+//         // Reset on active low reset
+//         for (i = 0; i < 8; i = i + 1) begin
+//             dropout_enabled_weights[i] <= 8'b0; // Initialize to 0
+//         end
+//     end else if (ui_ena) begin
+//         // Apply dropout based on the hyperparameter dropout_rate
+//         for (i = 0; i < 8; i = i + 1) begin
+//             if ($random < dropout_rate) begin
+//                 // Set output to 0 based on dropout rate
+//                 dropout_enabled_weights[i] <= 8'b0;
+//             end else begin
+//                 // Pass through the original weights
+//                 dropout_enabled_weights[i] <= dropout_weights[i];
+//             end
+//         end
+//     end
+// end
+
+// // Output the selected weights
+// assign uo_out[0] = dropout_enabled_weights[0];
+// assign uo_out[1] = dropout_enabled_weights[1];
+// assign uo_out[2] = dropout_enabled_weights[2];
+// assign uo_out[3] = dropout_enabled_weights[3];
+// assign uo_out[4] = dropout_enabled_weights[4];
+// assign uo_out[5] = dropout_enabled_weights[5];
+// assign uo_out[6] = dropout_enabled_weights[6];
+// assign uo_out[7] = dropout_enabled_weights[7];
+
+// endmodule
+
+
+// module dropout_module (
+//     input wire clk,      // Clock signal
+//     input wire rst_n,    // Reset signal
+//     input wire ui_ena,   // Enable signal used to determine if dropout should occur
+//     input wire [7:0] ui_in,
+//     output wire [7:0] uo_out
+// );
+
+// reg [7:0] dropout_weights [0:7]; // An array to store random dropout weights
+// wire [7:0] dropout_enabled_weights [0:7]; // An array to store dropout-enabled weights
+
+// parameter dropout_rate = 16'h8000; // Hyperparameter for dropout rate (e.g., 0.5 for 50% dropout)
+
+// // Generate random weights for each neuron
+// always @(posedge clk or negedge rst_n) begin
+//     if (!rst_n) begin
+//         // Reset on active low reset
+//         for (int i = 0; i < 8; i = i + 1) begin
+//             dropout_weights[i] <= 8'b0; // Initialize to 0
+//         end
+//     end else if (ui_ena) begin
+//         // Generate random weights for each neuron
+//         for (int i = 0; i < 8; i = i + 1) begin
+//             dropout_weights[i] <= $random; // Replace this with your own random weight generation logic
+//         end
+//     end
+// end
+
+// // Introduce dropout based on the hyperparameter dropout_rate
+// always @(posedge clk or negedge rst_n) begin
+//     if (!rst_n) begin
+//         // Reset on active low reset
+//         for (int i = 0; i < 8; i = i + 1) begin
+//             dropout_enabled_weights[i] <= 8'b0; // Initialize to 0
+//         end
+//     end
+// end
+
+// // Use another always block for the element-wise assignments
+// always @* begin
+//     for (int i = 0; i < 8; i = i + 1) begin
+//         if (ui_ena) begin
+//             if ($random < dropout_rate) begin
+//                 // Set output to 0 based on dropout rate
+//                 dropout_enabled_weights[i] = 8'b0;
+//             end else begin
+//                 // Pass through the original weights
+//                 dropout_enabled_weights[i] = dropout_weights[i];
+//             end
+//         end else begin
+//             // If ui_ena is 0, pass through the original weights
+//             dropout_enabled_weights[i] = dropout_weights[i];
+//         end
+//     end
+// end
+
+// // Output the selected weights
+// assign uo_out[0] = dropout_enabled_weights[0];
+// assign uo_out[1] = dropout_enabled_weights[1];
+// assign uo_out[2] = dropout_enabled_weights[2];
+// assign uo_out[3] = dropout_enabled_weights[3];
+// assign uo_out[4] = dropout_enabled_weights[4];
+// assign uo_out[5] = dropout_enabled_weights[5];
+// assign uo_out[6] = dropout_enabled_weights[6];
+// assign uo_out[7] = dropout_enabled_weights[7];
+
+// endmodule
+
+
+// module dropout_module (
+//     input wire clk,      // Clock signal
+//     input wire rst_n,    // Reset signal
+//     input wire ui_ena,   // Enable signal used to determine if dropout should occur
+//     input wire [7:0] ui_in,
+//     output wire [7:0] uo_out
+// );
+
+// reg [7:0] dropout_weights [0:7]; // An array to store random dropout weights
+// wire [7:0] dropout_enabled_weights [0:7]; // An array to store dropout-enabled weights
+
+// parameter dropout_rate = 4'b1000; // Hyperparameter for dropout rate (e.g., 0.5 for 50% dropout)
+
+// // Generate random weights for each neuron
+// always @(posedge clk or negedge rst_n) begin
+//     if (!rst_n) begin
+//         // Reset on active low reset
+//         for (int i = 0; i < 8; i = i + 1) begin
+//             dropout_weights[i] <= 8'b0; // Initialize to 0
+//         end
+//     end else if (ui_ena) begin
+//         // Generate random weights for each neuron
+//         for (int i = 0; i < 8; i = i + 1) begin
+//             dropout_weights[i] <= $random; // Replace this with your own random weight generation logic
+//         end
+//     end
+// end
+
+// // Introduce dropout based on the hyperparameter dropout_rate
+// always @(posedge clk or negedge rst_n) begin
+//     if (!rst_n) begin
+//         // Reset on active low reset
+//         for (int i = 0; i < 8; i = i + 1) begin
+//             dropout_enabled_weights[i] <= 8'b0; // Initialize to 0
+//         end
+//     end else if (ui_ena) begin
+//         // Apply dropout based on the hyperparameter dropout_rate
+//         for (int i = 0; i < 8; i = i + 1) begin
+//             if ($random < dropout_rate) begin
+//                 // Set output to 0 based on dropout rate
+//                 dropout_enabled_weights[i] <= 8'b0;
+//             end else begin
+//                 // Pass through the original weights
+//                 dropout_enabled_weights[i] <= dropout_weights[i];
+//             end
+//         end
+//     end
+// end
+
+// // Output the selected weights
+// assign uo_out[0] = dropout_enabled_weights[0];
+// assign uo_out[1] = dropout_enabled_weights[1];
+// assign uo_out[2] = dropout_enabled_weights[2];
+// assign uo_out[3] = dropout_enabled_weights[3];
+// assign uo_out[4] = dropout_enabled_weights[4];
+// assign uo_out[5] = dropout_enabled_weights[5];
+// assign uo_out[6] = dropout_enabled_weights[6];
+// assign uo_out[7] = dropout_enabled_weights[7];
+// endmodule
+
+// module RandomDropout (
+//     input wire clk,      // Clock signal for simulation
+//     input wire reset,    // Reset signal
+//     input wire enable,   // Enable signal used to determine if dropout should occur
+//     input wire [7:0] datain [0:7], // Data input for 8 neurons
+//     output reg [7:0] dataout [0:7] // Data output for 8 neurons
+// );
+
+// parameter dropoutrate = 0.5; // Dropout rate, can be changed as needed
+
+// reg [31:0] randomvalues [0:7]; // Array of random numbers, one per neuron
+
+// always @(posedge clk or posedge reset) begin
+//     if (reset) begin
+//         for (int i = 0; i < 8; i = i + 1) begin
+//             randomvalues[i] <= 32'h0;
+//         end
+//     end else if (enable) begin
+//         for (int i = 0; i < 8; i = i + 1) begin
+//             randomvalues[i] <= $urandom; // Use $urandom for uniform distribution
+//         end
+//     end
+// end
+
+// genvar i;
+// generate
+//     for (i = 0; i < 8; i = i + 1) begin : dropout_block
+//         always @* begin
+//             if (reset) begin
+//                 dataout[i] <= 8'h0; // Reset output to 0 for each neuron
+//             end else if (enable) begin
+//                 if (randomvalues[i] < dropoutrate * 2**32) begin
+//                     dataout[i] <= 8'h0; // Apply dropout
+//                 end else begin
+//                     dataout[i] <= datain[i]; // Pass data through
+//                 end
+//             end
+//         end
+//     end
+// endgenerate
+
+// endmodule
+
+
 // module RandomDropout (
 //     input wire clk,      // need a clock
 //     input wire reset,    // need reset as well
 //     input wire enable,   // enable used to determine if dropout should occur
 //     input wire [7:0] datain [0:7], // data in for 8 neurons
-//     output wire [7:0] dataout [0:7] // data out for 8 neurons
+//     output reg [7:0] dataout [0:7] // data out for 8 neurons
 // );
 
 // parameter dropoutrate = 0.5; // dropout rate can be changed, it represents the likelihood that a given value will be set to 0.
@@ -66,18 +321,21 @@ endmodule
 //     end
 // end
 
+// genvar i;
 // generate
-//     for (int i = 0; i < 8; i = i + 1) begin : dropout_block
+//     for (i = 0; i < 8; i = i + 1) begin : dropout_block
 //         always @* begin
 //             if (reset) begin
 //                 dataout[i] <= 8'h0; // Reset output to 0 for each neuron
-//             else if (enable) begin
+//             end else if (enable) begin
 //                 if (randomvalues[i] < dropoutrate * 2**32) begin
-//                     dataout[i] = 8'h0; // Apply dropout
+//                     dataout[i] <= 8'h0; // Apply dropout
 //                 end else begin
-//                     dataout[i] = datain[i]; // Pass data through
+//                     dataout[i] <= datain[i]; // Pass data through
 //                 end
 //             end
 //         end
 //     end
 // endgenerate
+
+// endmodule
--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -1,38 +1,22 @@
 `default_nettype none
-module tt_um_seven_segment_seconds #(
-    parameter MAX_COUNT = 24'd10_000_000
-) (
-    input wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-    input wire [7:0] uio_in,   // IOs: Bidirectional Input path
-    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-    input wire ena,      // will go high when the design is enabled
-    input wire clk,      // clock
-    input wire rst_n     // reset_n - low to reset
+
+module tt_um_seven_segment_seconds (
+    input wire clk,      // Clock signal
+    input wire rst_n,    // Reset signal
+    input wire ui_ena,   // Enable signal used to determine if dropout should occur
+    input wire [7:0] ui_in, // Data input for 8 neurons
+    output wire [7:0] uo_out, // Data output for 8 neurons
+    output wire [7:0] uio_out, // Data output for 8 neurons
+    output wire [7:0] uio_oe // Output enable for 8 neurons
 );
 
-wire [7:0] dropoutdataout [0:7]; // Declare an array of wires to hold the dropout output
-wire [7:0] datain [0:7]; // Declare an array of wires to connect to datain
-
-assign datain[0] = ui_in;
-assign datain[1] = ui_in;
-assign datain[2] = ui_in;
-assign datain[3] = ui_in;
-assign datain[4] = ui_in;
-assign datain[5] = ui_in;
-assign datain[6] = ui_in;
-assign datain[7] = ui_in;
-
-RandomDropout dropout_inst (
-    .clk(clk),
-    .reset(rst_n),
-    .enable(ena),
-    .datain(datain),
-    .dataout(dropoutdataout)
+dropout_module RandomDropout (
+    .clk(clk),        // Connect clk from top module to dropout module
+    .rst_n(rst_n),    // Connect rst_n from top module to dropout module
+    .ui_ena(ui_ena),  // Connect ui_ena from top module to dropout module
+    .ui_in(ui_in),    // Connect ui_in from top module to dropout module
+    .uo_out(uo_out)   // Connect uo_out from dropout module to uo_out in the top module
 );
-
-assign uo_out = dropoutdataout[0]; // Connect the first neuron's dropout output to uo_out
 
 endmodule
 // module tt_um_seven_segment_seconds #(
@@ -48,39 +32,17 @@ endmodule
 //     input wire rst_n     // reset_n - low to reset
 // );
 
-// wire [7:0] dropoutdataout; // Declare a wire to hold the dropout output
-// reg [7:0] datain; // Create a register to connect to datain
+// wire [7:0] dropoutdataout [0:7]; // Declare an array of wires to hold the dropout output
+// wire [7:0] datain [0:7]; // Declare an array of wires to connect to datain
 
-// assign datain = ui_in; // Connect ui_in to datain
-
-// RandomDropout dropout_inst (
-//     .clk(clk),
-//     .reset(rst_n),
-//     .enable(ena),
-//     .datain(datain),
-//     .dataout(dropoutdataout)
-// );
-
-// assign uo_out = dropoutdataout; // Connect the dropout output to your output
-
-// endmodule
-// module tt_um_seven_segment_seconds #(
-//     parameter MAX_COUNT = 24'd10_000_000
-// ) (
-//     input wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-//     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-//     input wire [7:0] uio_in,   // IOs: Bidirectional Input path
-//     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-//     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-//     input wire ena,      // will go high when the design is enabled
-//     input wire clk,      // clock
-//     input wire rst_n     // reset_n - low to reset
-// );
-
-// wire [7:0] dropoutdataout; // Declare a wire to hold the dropout output
-// wire [7:0] datain [0:7]; // Create a wire array to connect to datain
-
-// assign datain = ui_in; // Connect ui_in to datain
+// assign datain[0] = ui_in;
+// assign datain[1] = ui_in;
+// assign datain[2] = ui_in;
+// assign datain[3] = ui_in;
+// assign datain[4] = ui_in;
+// assign datain[5] = ui_in;
+// assign datain[6] = ui_in;
+// assign datain[7] = ui_in;
 
 // RandomDropout dropout_inst (
 //     .clk(clk),
@@ -90,35 +52,6 @@ endmodule
 //     .dataout(dropoutdataout)
 // );
 
-// assign uo_out = dropoutdataout; // Connect the dropout output to your output
+// assign uo_out = dropoutdataout[0]; // Connect the first neuron's dropout output to uo_out
 
 // endmodule
-// module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
-//     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-//     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-//     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
-//     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-//     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-//     input  wire       ena,      // will go high when the design is enabled
-//     input  wire       clk,      // clock
-//     input  wire       rst_n     // reset_n - low to reset
-// );
-
-   
-// wire [7:0] dropoutdataout [0:7];
-// // assign ou_out = dropout_inst.dataout;
-// // wire [N-1:0] dropoutdatain;
-// // assign dropoutdatain = ui_in;
-
-// assign uo_out = dropoutdataout;
-
-// RandomDropout dropout_inst [0:7] (
-//     .clk(clk),
-//     .reset(rst_n),
-//     .enable(ena),
-//     .datain(ui_in),
-//     .dataout(dropoutdataout)
-//     );
-
-    
-
