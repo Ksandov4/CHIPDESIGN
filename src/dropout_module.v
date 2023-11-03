c4076fefe0ecea66f10e0d0058b863989d14658a
ksandov4 <ksandov4@ucsc.edu>
2023-11-03 15:39:55 -0700

    yesss

--- src/dropout_module.v
+++ src/dropout_module.v
@@ -13,6 +13,11 @@ reg [7:0] dropout_enabled_weights [0:7]; // An array to store dropout-enabled we
 
 parameter dropout_rate = 8'b10000000; // Hyperparameter for dropout rate (e.g., 0.5 for 50% dropout)
 
+// Function to generate a random number
+function [7:0] my_urandom;
+    my_urandom = $urandom;
+endfunction
+
 // Generate random weights for each neuron
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
@@ -21,9 +26,9 @@ always @(posedge clk or negedge rst_n) begin
             dropout_weights[i] <= 8'b0; // Initialize to 0
         end
     end else if (ui_ena) begin
-        // Generate random weights for each neuron
+        // Generate random weights for each neuron using my_urandom
         for (int i = 0; i < 8; i = i + 1) begin
-            dropout_weights[i] <= $urandom; // Replace this with your own random weight generation logic
+            dropout_weights[i] <= my_urandom;
         end
     end
 end
@@ -38,7 +43,7 @@ always @(posedge clk or negedge rst_n) begin
     end else if (ui_ena) begin
         // Apply dropout based on the hyperparameter dropout_rate
         for (int i = 0; i < 8; i = i + 1) begin
-            if ($urandom < dropout_rate) begin
+            if (my_urandom < dropout_rate) begin
                 // Set output to 0 based on dropout rate
                 dropout_enabled_weights[i] <= 8'b0;
             end else begin
@@ -61,6 +66,67 @@ assign uo_out[7] = dropout_enabled_weights[7];
 
 endmodule
 
+// module dropout_module (
+//     input wire clk,      // Clock signal
+//     input wire rst_n,    // Reset signal
+//    input wire ui_ena,   // Enable signal used to determine if dropout should occur
+//     input wire [7:0] ui_in,
+//     output wire [7:0] uo_out
+// );
+
+// reg [7:0] dropout_weights [0:7]; // An array to store random dropout weights
+// reg [7:0] dropout_enabled_weights [0:7]; // An array to store dropout-enabled weights
+
+// parameter dropout_rate = 8'b10000000; // Hyperparameter for dropout rate (e.g., 0.5 for 50% dropout)
+
+// // Generate random weights for each neuron
+// always @(posedge clk or negedge rst_n) begin
+//     if (!rst_n) begin
+//         // Reset on active low reset
+//         for (int i = 0; i < 8; i = i + 1) begin
+//             dropout_weights[i] <= 8'b0; // Initialize to 0
+//         end
+//     end else if (ui_ena) begin
+//         // Generate random weights for each neuron
+//         for (int i = 0; i < 8; i = i + 1) begin
+//             dropout_weights[i] <= $urandom; // Replace this with your own random weight generation logic
+//         end
+//     end
+// end
+
+// // Introduce dropout based on the hyperparameter dropout_rate
+// always @(posedge clk or negedge rst_n) begin
+//     if (!rst_n) begin
+//         // Reset on active low reset
+//         for (int i = 0; i < 8; i = i + 1) begin
+//             dropout_enabled_weights[i] <= 8'b0; // Initialize to 0
+//         end
+//     end else if (ui_ena) begin
+//         // Apply dropout based on the hyperparameter dropout_rate
+//         for (int i = 0; i < 8; i = i + 1) begin
+//             if ($urandom < dropout_rate) begin
+//                 // Set output to 0 based on dropout rate
+//                 dropout_enabled_weights[i] <= 8'b0;
+//             end else begin
+//                 // Pass through the original weights
+//                 dropout_enabled_weights[i] <= dropout_weights[i];
+//             end
+//         end
+//     end
+// end
+
+// // Output the selected weights
+// assign uo_out[0] = dropout_enabled_weights[0];
+// assign uo_out[1] = dropout_enabled_weights[1];
+// assign uo_out[2] = dropout_enabled_weights[2];
+// assign uo_out[3] = dropout_enabled_weights[3];
+// assign uo_out[4] = dropout_enabled_weights[4];
+// assign uo_out[5] = dropout_enabled_weights[5];
+// assign uo_out[6] = dropout_enabled_weights[6];
+// assign uo_out[7] = dropout_enabled_weights[7];
+
+// endmodule
+ 
 
 // module dropout_module (
 //     input wire clk,      // Clock signal
