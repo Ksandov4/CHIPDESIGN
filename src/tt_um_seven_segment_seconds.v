e8f05769cb1db920fa9af86bbc8593de8cbc9530
ksandov4 <ksandov4@ucsc.edu>
2023-11-02 21:20:42 -0700

    please fukin woek

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -1,5 +1,7 @@
 `default_nettype none
-module tt_um_seven_segment_seconds (
+module tt_um_seven_segment_seconds #(
+    parameter MAX_COUNT = 24'd10_000_000
+) (
     input wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input wire [7:0] uio_in,   // IOs: Bidirectional Input path
@@ -11,19 +13,19 @@ module tt_um_seven_segment_seconds (
 );
 
 wire [7:0] dropoutdataout [0:7]; // Declare an array of wires to hold the dropout output
-reg [7:0] datain; // Create a register to connect to datain
+wire [7:0] datain [0:7]; // Declare an array of wires to connect to datain
 
-assign datain = ui_in; // Connect ui_in to datain
+assign datain = {ui_in, ui_in, ui_in, ui_in, ui_in, ui_in, ui_in, ui_in}; // Assign data to each element in the array
 
 RandomDropout dropout_inst [0:7] (
     .clk(clk),
     .reset(rst_n),
     .enable(ena),
-    .datain({datain, datain, datain, datain, datain, datain, datain, datain}),
+    .datain(datain),
     .dataout(dropoutdataout)
 );
 
-assign uo_out = dropoutdataout; // Connect the dropout output to your output
+assign uo_out = dropoutdataout[0]; // Connect the first neuron's dropout output to uo_out
 
 endmodule
 // module tt_um_seven_segment_seconds #(
